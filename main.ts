32ead3c69a2627eaa8fc00712c2073dd2b0cf675
dsanehl321 <124673810+dsanehl321@users.noreply.github.com>
2023-02-21 11:28:50 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,64 @@
+input.onButtonPressed(Button.A, function () {
+    if (pulsado == false) {
+        pulsado = true
+        if (num_icono == 0 || num_icono == 2) {
+            puntos_A += 1
+            if (puntos_A == 10) {
+                basic.showString("A: has ganado")
+                juego_detenido = true
+                basic.pause(5000)
+            }
+        } else if (num_icono == 8) {
+            basic.showString("A: has perdido")
+            juego_detenido = true
+            basic.pause(5000)
+        } else {
+            if (puntos_A > 0) {
+                puntos_A += -1
+            }
+        }
+    }
+})
+function partida_nueva () {
+    juego_detenido = false
+    puntos_A = 0
+    puntos_B = 0
+    pulsado = false
+}
+input.onButtonPressed(Button.AB, function () {
+    partida_nueva()
+})
+input.onButtonPressed(Button.B, function () {
+    if (pulsado == false) {
+        pulsado = true
+        if (num_icono == 0 || num_icono == 2) {
+            puntos_B += 1
+            if (puntos_B == 10) {
+                basic.showString("A: has ganado")
+                juego_detenido = true
+                basic.pause(5000)
+            }
+        } else if (num_icono == 8) {
+            basic.showString("A: has perdido")
+            juego_detenido = true
+            basic.pause(5000)
+        } else {
+            if (puntos_B > 0) {
+                puntos_B += -1
+            }
+        }
+    }
+})
 function cambia_icono () {
     num_icono = randint(0, 9)
     lista[num_icono].showImage(0)
-    basic.pause(200)
+    basic.pause(250)
 }
+let puntos_B = 0
+let juego_detenido = false
+let puntos_A = 0
 let num_icono = 0
+let pulsado = false
 let lista: Image[] = []
 lista = [
 images.iconImage(IconNames.Heart),
@@ -17,6 +72,9 @@ images.iconImage(IconNames.No),
 images.iconImage(IconNames.Skull),
 images.iconImage(IconNames.EigthNote)
 ]
+partida_nueva()
 basic.forever(function () {
-    cambia_icono()
+    if (!(juego_detenido)) {
+        cambia_icono()
+    }
 })
